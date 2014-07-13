3fddf4b822eee107df3dd7473c42a89c671c1d13
Leonardo M. Ramé <martinrame@yahoo.com>
2014-07-13 12:02:46 -0300

    Added deploy file

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+// deploy to cpvs
+scp cgi-bin/tir cpvs:/home/leonardo/offirad/cgi-bin
+scp js/*.js cpvs:/home/leonardo/offirad/js
+scp js/controllers/*.js cpvs:/home/leonardo/offirad/js/controllers
+scp css/*.css cpvs:/home/leonardo/offirad/css
+scp html/*.html cpvs:/home/leonardo/offirad/html
+scp html/partials/*.html cpvs:/home/leonardo/offirad/html/partials
