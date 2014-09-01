c15d716217ce44ae8d5b5d1673fef6d9aecef571
Leonardo M. Ramé <martinrame@yahoo.com>
2014-09-01 20:30:58 -0300

    Changes

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ scp cgi/tir cpvs:/home/leonardo/offirad/cgi
 scp js/*.js cpvs:/home/leonardo/offirad/js
 scp js/controllers/*.js cpvs:/home/leonardo/offirad/js/controllers
 scp css/*.css cpvs:/home/leonardo/offirad/css
+scp swf/*.swf cpvs:/home/leonardo/offirad/swf
 scp html/*.html cpvs:/home/leonardo/offirad/html
 scp html/partials/*.html cpvs:/home/leonardo/offirad/html/partials
