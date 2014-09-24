6595e6ffc6d6e96b541be4627e0bd7637686bbc5
Leonardo M. Ramé <martinrame@yahoo.com>
2014-09-24 18:37:36 -0300

    Fixed errors in query

--- sql/20140924.sql
+++ sql/20140924.sql
@@ -1,8 +1,8 @@
 create table gridfilters(
   idgridfilter serial not null,
   grid varchar(50),
-  column varchar(50),
-  iduser integer references(users),
+  field varchar(50),
+  iduser integer references users(iduser),
   filter varchar(50),
   primary key(idgridfilter));
 
