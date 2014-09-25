9fb4a0eb5c98d5310937bcb84030627f4420835e
Leonardo M. Ramé <martinrame@yahoo.com>
2014-09-25 07:26:33 -0300

    Removed 20140924.sql

--- sql/20140924.sql
+++
@@ -1,8 +0,0 @@
-create table gridfilters(
-  idgridfilter serial not null,
-  grid varchar(50),
-  field varchar(50),
-  iduser integer references users(iduser),
-  filter varchar(50),
-  primary key(idgridfilter));
-
