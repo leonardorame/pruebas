2f71cc3e8e86b84393de241faaeeede26ef71f09
Leonardo M. Ramé <leonardo@leonardo-SATELLITE-C50-A>
2014-10-02 17:52:45 -0300

    Implemented audio conversion to mp3

--- cgi/actaudio.pas
+++ cgi/actaudio.pas
@@ -6,8 +6,10 @@ unit actaudio;
 interface
 
 uses
-  Classes,BrookAction, BrookHttpDefs, BrookConsts, BrookUtils, SysUtils,
+  Classes,BrookAction, BrookConsts, BrookUtils, SysUtils,
   BrookLogger,
+  Math,
+  process,
   strutils,
   dmdatabase;
 
@@ -46,11 +48,14 @@ procedure TActAudio.Get;
 var
   lWav: TMemoryStream;
   lIdStudyProcedure: string;
+  lProcess: TProcess;
+  lBuf: array[0..511] of byte;
+  lReadCount: Integer;
 begin
   lIdStudyProcedure := Variable['filename'];
   lIdStudyProcedure := AnsiReplaceStr(lIdStudyProcedure, '.wav', '');
 
-  lWav := TMemoryStream.Create;
+  {lWav := TMemoryStream.Create;
   try
     if datamodule1.LoadWav(lWav, StrToInt(lIdStudyProcedure)) then
     begin
@@ -63,6 +68,52 @@ begin
     else
       Write('No audio');
   finally
+    lWav.Free;
+  end;}
+
+  // obtenemos el WAV y lo convertimos a MP3.
+  lWav := TMemoryStream.Create;
+  try
+    lProcess := TProcess.Create(nil);
+    if datamodule1.LoadWav(lWav, StrToInt(lIdStudyProcedure)) then
+    begin
+      lProcess.Executable := '/usr/bin/lame';
+      lProcess.Parameters.Add('-');  // el primer - es lectura por stdin
+      lProcess.Parameters.Add('-');  // el 2do - es escritura eb stdout
+      lProcess.Options := [poUsePipes];
+      lProcess.Execute;
+      lWav.Position:= 0;
+      lWav.SaveToStream(lProcess.Input);
+      lProcess.CloseInput;
+
+      lWav.Clear;
+      while lProcess.Running or (lProcess.Output.NumBytesAvailable > 0) do
+      begin
+        {while lProcess.Stderr.NumBytesAvailable > 0 do
+        begin
+          lReadCount := Min(512, lProcess.Stderr.NumBytesAvailable);
+          lProcess.Stderr.Read(lCharBuffer, lReadCount);
+          lErrStream.Write(lCharBuffer, lReadCount);
+        end;}
+        while lProcess.Output.NumBytesAvailable > 0 do
+        begin
+          lReadCount := Min(512, lProcess.Output.NumBytesAvailable);
+          lProcess.Output.Read(lBuf, lReadCount);
+          lWav.Write(lBuf, lReadCount);
+        end;
+      end;
+
+      lWav.Position:= 0;
+
+      HttpResponse.ContentStream := lWav;
+      HttpResponse.ContentLength:= lWav.Size;
+      HttpResponse.ContentType := 'audio/mpeg';
+      HttpResponse.SendContent;
+    end
+    else
+      Write('No audio');
+  finally
+    lProcess.Free;
     lWav.Free;
   end;
 end;
